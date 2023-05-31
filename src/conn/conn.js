0815e12dd0ab98eb833010bfb95eeccda13ae665
tejas katkade <tejas2002rk@gmail.com>
2023-05-31 18:30:59 +0530

    s

--- src/conn/conn.js
+++ src/conn/conn.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const mongoose= require('mongoose');
 
 
-
-
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("DB Connected");
 }).catch((err)=> console.log(err));
